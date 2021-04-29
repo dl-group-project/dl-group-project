ba0637838ea6b4bd265fc72052948830e4c45bca
Sreedhar Radhakrishnan <sreedhar@Sreedhars-MacBook-Pro.local>
2021-04-29 08:42:57 -0400

    Added Project Utilities

---
+++ utilities/utilities.py
@@ -0,0 +1,2 @@
+# PHONEME_MAPPER is a dictionary that maps the integer phoneme label assigned during voice activity detection to the actual phoneme sound string
+PHONEME_MAPPER = {0: 'SIL', 1: 'AE', 2: 'AH', 3: 'AW', 4: 'AY', 5: 'B', 6: 'EH', 7: 'D', 8: 'DH', 9: 'EE', 10: 'FF', 11: 'G', 12: 'HH', 13: 'IH', 14: 'II', 15: 'J', 16: 'K', 17: 'LL', 18: 'MM', 19: 'NN', 20: 'OH', 21: 'OO', 22: 'OW', 23: 'OY', 24: 'P', 25: 'RR', 26: 'SH', 27: 'SS', 28: 'T', 29: 'TH', 30: 'UE', 31: 'UH', 32: 'VV', 33: 'WW', 34: 'YY', 35: 'ZZ'}
