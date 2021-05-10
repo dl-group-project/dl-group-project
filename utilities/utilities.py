ea2a464c70ae0092363249d243ede5d8a7242d7c
Marta Mendez Simon <martamensi@MacBook-Pro-de-Marta.local>
2021-05-10 00:28:13 -0400

    modify specified task classifiers, 3 outputs

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -4,52 +4,65 @@ PHONEME_MAPPER = {0: 'SIL', 1: 'AE', 2: 'AH', 3: 'AW', 4: 'AY', 5: 'B', 6: 'EH',
 # Baker's categories
 VOWELS_LIST = ['EE', 'IH', 'EH', 'AE', 'UH', 'ER', 'AH', 'AW', 'OO', 'UE']  # deliberately leaving out diphthongs
 CONSONANTS_LIST = ['FF', 'HH', 'MM', 'NN', 'NG', 'RR', 'SS', 'SH', 'VV', 'WW', 'YY', 'ZZ']  # leaving out semivowels and stops
+TASK1_NONE = [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in VOWELS_LIST + CONSONANTS_LIST]
 
 HIGH_VOWELS_LIST = ['EE', 'IH', 'UE', 'OO']
 LOW_VOWELS_LIST = ['AE', 'AH', 'AW']
+TASK3_NONE = [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in HIGH_VOWELS_LIST + LOW_VOWELS_LIST]
 
 VOICED_FRICATIVES = ['DH', 'VV', 'ZZ']
 UNVOICED_FRICATIVES = ['FF', 'SS', 'SH', 'TH']
+TASK4_NONE = [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in VOICED_FRICATIVES + UNVOICED_FRICATIVES]
 
 SPECIALIZED_TASKS = {
     '1_vowel_vs_consonant': {
-        0: VOWELS_LIST,
-        1: CONSONANTS_LIST
+        0: TASK1_NONE,
+        1: VOWELS_LIST,
+        2: CONSONANTS_LIST
     },
     '3_highvowel_vs_lowvowel': {
-        0: HIGH_VOWELS_LIST,
-        1: LOW_VOWELS_LIST
+        0: TASK3_NONE,
+        1: HIGH_VOWELS_LIST,
+        2: LOW_VOWELS_LIST
     },
     '4_voiced_vs_unvoiced_fricatives': {
-        0: VOICED_FRICATIVES,
-        1: UNVOICED_FRICATIVES
+        0: TASK4_NONE,
+        1: VOICED_FRICATIVES,
+        2: UNVOICED_FRICATIVES
     },
     '5_ss_vs_zz': {
-        0: ['SS'],
-        1: ['ZZ']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["SS", "ZZ"]],
+        1: ['SS'],
+        2: ['ZZ']
     },
     '6_b_vs_p': {
-        0: ['B'],
-        1: ['P']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["B", "P"]],
+        1: ['B'],
+        2: ['P']
     },
     '7_dh_vs_th': {
-        0: ['DH'],
-        1: ['TH']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["DH", "TH"]],
+        1: ['DH'],
+        2: ['TH']
     },
     '8_ww_vs_yy': {
-        0: ['WW'],
-        1: ['YY']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["WW", "YY"]],
+        1: ['WW'],
+        2: ['YY']
     },
     '9_ee_vs_aw': {
-        0: ['EE'],
-        1: ['AW']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["EE", "AW"]],
+        1: ['EE'],
+        2: ['AW']
     },
     '10_ah_vs_aw': {
-        0: ['AH'],
-        1: ['AW']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["AH", "AW"]],
+        1: ['AH'],
+        2: ['AW']
     },
     '11_mm_vs_nn': {
-        0: ['MM'],
-        1: ['NN']
+        0: [phoneme_tag for phoneme_index, phoneme_tag in PHONEME_MAPPER.items() if phoneme_tag not in ["MM", "NN"]],
+        1: ['MM'],
+        2: ['NN']
     },
 }
